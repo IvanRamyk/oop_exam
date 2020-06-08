fc1f283366b847297031be5befe48595227dd2c9
IvanRamyk <ivan.ramyk.official@gmail.com>
2020-06-08 17:45:39 +0300

    DateTime implemented

---
+++ src/Date/Date.hpp
@@ -0,0 +1,8 @@
+//
+// Created by ivan on 08.06.20.
+//
+
+#ifndef OOP_EXAM_DATE_HPP
+#define OOP_EXAM_DATE_HPP
+
+#endif //OOP_EXAM_DATE_HPP
---
+++ tests/DateTimeTests.cpp
@@ -0,0 +1,4 @@
+//
+// Created by ivan on 08.06.20.
+//
+
