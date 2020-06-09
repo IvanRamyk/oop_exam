f359843896350d7b049d84551e9245a931ef5c81
hryhorchuk117 <hryhorchuk117@gmail.com>
2020-06-10 02:15:06 +0300

    heap && merge

--- src/Sort/Sort.cpp
+++ src/Sort/Sort.cpp
@@ -27,3 +27,64 @@ void Sort<T>::selectionSort(const std::function<bool(T, T)>& compare) {
         std::swap(min, _data[i]);
     }
 }
+
+template<class T>
+void Sort<T>::heapSort(const std::function<bool(T, T)> &compare) {
+    for (int j = 0; j < _data.size(); j++) {
+        for (int i = _data.size() / 2 -1 - j/2; i > -1; i--) {
+            if (2 * i + 2 <= _data.size() - 1-j) {
+                if (compare(_data[2 * i + 2], _data[2 * i + 1])) {
+                    if (compare(_data[i], _data[2 * i + 1])) {
+                        std::swap(_data[i], _data[2 * i + 1]);
+                    }
+                }
+                else
+                if (compare(_data[i], _data[2 * i + 1])) {
+                    std::swap(_data[i], _data[2 * i + 2]);
+                }
+            }
+            else
+            if (2 * i + 1 <= _data.size() - 1-j)
+                if (compare(_data[i], _data[2 * i + 1]))
+                    std::swap(_data[i], _data[2 * i + 1]);
+        }
+        std::swap(_data[0], _data[_data.size() - 1 - j]);
+    }
+}
+
+template<class T>
+void Sort<T>::mergeSort(const std::function<bool(T, T)> &compare) {
+    std::vector<T> new_vec = _data;
+
+    std::vector<T> temp(new_vec.size());
+    mergePart(new_vec, temp, compare);
+
+    for (int j = 0; j < _data.size(); ++j) {
+        _data[j] = new_vec[j];
+    }
+}
+
+template <class T>
+void Sort<T>::merge(std::vector<T>& vec, int iLeft, int iRight, int iEnd, std::vector<T>& temp, const std::function<bool(T, T)> &compare) {
+    int i = iLeft, j = iRight;
+    for (int k = iLeft; k < iEnd; k++) {
+        if (i < iRight && (j >= iEnd || compare(vec[i], vec[j]))) {
+            temp[k]= vec[i];
+            i = i + 1;
+        } else {
+            temp[k] = vec[j];
+            j = j + 1;
+        }
+    }
+}
+
+template <class T>
+void Sort<T>::mergePart(std::vector<T>& vec, std::vector<T>& temp, const std::function<bool(T, T)> &compare) {
+    for (int width = 1; width < vec.size(); width = 2 * width) {
+        for (int i = 0; i < vec.size(); i = i + 2 * width) {
+            merge(vec, i, std::min(i + width, (int)vec.size()),
+                  std::min(i + 2 * width, (int)vec.size()), temp, compare);
+        }
+        vec = temp;
+    }
+}
--- src/Sort/Sort.h
+++ src/Sort/Sort.h
@@ -14,9 +14,17 @@ public:
 
     void selectionSort(const std::function<bool(T, T)>& compare);
 
+    void heapSort(const std::function<bool(T, T)>& compare);
+
+    void mergeSort(const std::function<bool(T, T)>& compare);
+
     std::vector<T> getData() const;
 private:
     std::vector<T> _data;
+
+    void merge(std::vector<T>& vec, int iLeft, int iRight, int iEnd, std::vector<T>& temp, const std::function<bool(T, T)> &compare);
+
+    void mergePart(std::vector<T>& vec, std::vector<T>& temp, const std::function<bool(T, T)> &compare);
 };
 
 
