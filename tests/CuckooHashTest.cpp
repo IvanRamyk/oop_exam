eb43ab38fbc1043d9e3116e37d187e4ba1f7267a
IvanRamyk <ivan.ramyk.official@gmail.com>
2020-06-10 18:08:31 +0300

    Set<Hash<T>> implted

--- tests/CuckooHashTest.cpp
+++ tests/CuckooHashTest.cpp
@@ -20,3 +20,12 @@ TEST(Hash, Cuckoo2) {
     auto n = table.getData();
     EXPECT_TRUE(table.getData() == n);
 }
+
+TEST(Hash, Cuckoo3){
+    CuckooHash<date_time::Time> s;
+    s.add(date_time::Time(1,1,1));
+    EXPECT_TRUE(s.search(date_time::Time(1,1,1)) != s.getData().first.end());
+    EXPECT_FALSE(s.search(date_time::Time(1,3,1)) != s.getData().first.end());
+    s.erase(date_time::Time(1,1,1));
+    //EXPECT_FALSE(s.count(date_time::Time(1,1,1)));
+}
